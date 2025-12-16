c25a6c1e9c989faa21c2c29d53eab41aa605b43b
Haniyah09 <niihanii142@gmail.com>
2025-12-16 10:22:50 +0700

    Add files via upload

---
+++ praktikum9/coding/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp_host' => 'smtp.gmail.com',
+  'smtp_port' => 587,
+  'smtp_user' => 'niihanii142@gmail.com',
+  'smtp_pass' => 'aewxbdvgxeibvylf',
+  'from_email' => 'niihanii142@gmail.com',
+  'from_name'  => 'Sistem Data Mahasiswa',
+  'to_email'   => 'niifashiononline3109@gmail.com',
+  'to_name'    => 'Penerima'
+];
